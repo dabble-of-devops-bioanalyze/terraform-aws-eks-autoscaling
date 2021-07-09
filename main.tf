3055a875d57d676e82f38bb84508e363a30bdcdc
Jillian Rowe <jillian.e.rowe@gmail.com>
2021-07-09 15:46:56 -0400

    trying this out

--- main.tf
+++ main.tf
@@ -1,13 +1,138 @@
-resource "random_integer" "example" {
-  count = module.this.enabled ? 1 : 0
+provider "aws" {
+  region = var.region
+}
 
-  min = 1
-  max = 50000
-  keepers = {
-    example = var.example
+data "aws_availability_zones" "available" {}
+
+data "aws_caller_identity" "current" {}
+
+module "label" {
+  source     = "cloudposse/label/null"
+  version    = "0.24.1"
+  attributes = ["cluster"]
+
+  tags = {
+    "k8s.io/cluster-autoscaler/${module.this.cluster_name}" = "true"
+    "k8s.io/cluster-autoscaler/enabled" = "true"
   }
+
+  context = module.this.context
 }
 
 locals {
-  example = format("%v %v", var.example, join("", random_integer.example[*].result))
+  # The usage of the specific kubernetes.io/cluster/* resource tags below are required
+  # for EKS and Kubernetes to discover and manage networking resources
+  # https://www.terraform.io/docs/providers/aws/guides/eks-getting-started.html#base-vpc-networking
+
+  tags = merge(module.label.tags, 
+    map("kubernetes.io/cluster/${module.label.id}", "shared"))
+
+  # Unfortunately, most_recent (https://github.com/cloudposse/terraform-aws-eks-workers/blob/34a43c25624a6efb3ba5d2770a601d7cb3c0d391/main.tf#L141)
+  # variable does not work as expected, if you are not going to use custom ami you should
+  # enforce usage of eks_worker_ami_name_filter variable to set the right kubernetes version for EKS workers,
+  # otherwise will be used the first version of Kubernetes supported by AWS (v1.11) for EKS workers but
+  # EKS control plane will use the version specified by kubernetes_version variable.
+  eks_worker_ami_name_filter = "amazon-eks-node-${var.kubernetes_version}*"
+
+  # required tags to make ALB ingress work https://docs.aws.amazon.com/eks/latest/userguide/alb-ingress.html
+  public_subnets_additional_tags = {
+    "kubernetes.io/role/elb" : 1
+  }
+  private_subnets_additional_tags = {
+    "kubernetes.io/role/internal-elb" : 1
+  }
+}
+
+module "eks_cluster" {
+  source = "cloudposse/eks-cluster/aws"
+  version     = "0.41.0"
+
+  region                       = var.region
+  vpc_id                       = module.vpc.vpc_id
+  subnet_ids                   = concat(module.subnets.private_subnet_ids, module.subnets.public_subnet_ids)
+  kubernetes_version           = var.kubernetes_version
+  local_exec_interpreter       = var.local_exec_interpreter
+  oidc_provider_enabled        = var.oidc_provider_enabled
+  enabled_cluster_log_types    = var.enabled_cluster_log_types
+  cluster_log_retention_period = var.cluster_log_retention_period
+
+  cluster_encryption_config_enabled                         = var.cluster_encryption_config_enabled
+  cluster_encryption_config_kms_key_id                      = var.cluster_encryption_config_kms_key_id
+  cluster_encryption_config_kms_key_enable_key_rotation     = var.cluster_encryption_config_kms_key_enable_key_rotation
+  cluster_encryption_config_kms_key_deletion_window_in_days = var.cluster_encryption_config_kms_key_deletion_window_in_days
+  cluster_encryption_config_kms_key_policy                  = var.cluster_encryption_config_kms_key_policy
+  cluster_encryption_config_resources                       = var.cluster_encryption_config_resources
+
+  context = module.this.context
+}
+
+module "eks_workers" {
+  source = "cloudposse/eks-workers/aws"
+  # version = "0.19.0"
+
+  for_each = var.eks_worker_groups
+
+  subnet_ids        = var.private_subnet_ids
+  cluster_name                       = module.label.id
+  # cluster_name      = data.null_data_source.wait_for_cluster_and_kubernetes_configmap.outputs["cluster_name"]
+  name = var.eks_worker_groups[each.key].name
+  instance_types    = var.eks_worker_groups[each.key].instance_type
+  desired_size      = var.eks_worker_groups[each.key].desired_size
+  min_size          = var.eks_worker_groups[each.key].min_size
+  max_size          = var.eks_worker_groups[each.key].max_size
+  disk_size         = var.eks_worker_groups[each.key].disk_size
+  kubernetes_labels = var.kubernetes_labels
+
+  tags = local.tags
+
+  bootstrap_extra_args               = "--use-max-pods false"
+  kubelet_extra_args                 = "--node-labels=purpose=ci-worker"
+
+  context = module.this.context
+
+  security_group_rules = [
+      {
+        type                     = "egress"
+        from_port                = 0
+        to_port                  = 65535
+        protocol                 = "-1"
+        cidr_blocks              = ["0.0.0.0/0"]
+        source_security_group_id = null
+        description              = "Allow all outbound traffic"
+      },
+      {
+        type                     = "ingress"
+        from_port                = 0
+        to_port                  = 65535
+        protocol                 = "-1"
+        cidr_blocks              = []
+        source_security_group_id = var.eks_worker_security_group_id 
+        description              = "Allow all inbound traffic from Security Group ID of the EKS cluster"
+      }
+    ]
+
+    # Auto-scaling policies and CloudWatch metric alarms
+    autoscaling_policies_enabled           = var.autoscaling_policies_enabled
+    cpu_utilization_high_threshold_percent = var.cpu_utilization_high_threshold_percent
+    cpu_utilization_low_threshold_percent  = var.cpu_utilization_low_threshold_percent
+}
+
+module "eks_node_group" {
+  source  = "cloudposse/eks-node-group/aws"
+  version = "0.19.0"
+
+  for_each = var.eks_node_groups
+  subnet_ids        = var.private_subnet_ids
+  cluster_name      = data.null_data_source.wait_for_cluster_and_kubernetes_configmap.outputs["cluster_name"]
+  instance_types    = var.eks_node_groups[each.key].instance_types
+  desired_size      = var.eks_node_groups[each.key].desired_size
+  min_size          = var.eks_node_groups[each.key].min_size
+  max_size          = var.eks_node_groups[each.key].max_size
+  disk_size         = var.eks_node_groups[each.key].disk_size
+
+  kubernetes_labels = var.kubernetes_labels
+  tags = local.tags
+
+  cluster_autoscaler_enabled = true
+  context = module.this.context
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,193 @@
-variable "example" {
-  description = "Example variable"
-  default     = "hello world"
+variable "region" {
+  type        = string
+  description = "AWS Region"
+}
+
+variable "availability_zones" {
+  type        = list(string)
+  description = "List of availability zones"
+}
+
+####################################################################
+# VPC 
+####################################################################
+
+variable "vpc_id" {
+  type = string
+  description = "VPC ID for the cluster VPC ID"
+}
+
+variable "private_subnet_ids" {
+  type = list(string)
+  description = "Private Subnet Ids"
+}
+
+variable "public_subnet_ids" {
+  type = list(string)
+  description = "Public Subnet Ids"
+}
+
+variable "eks_worker_security_group_id" {
+  type = string
+  description = "Security group to allow traffic eks workers to talk to eachother sg-XXXXXXXXX"
+}
+
+variable "kubernetes_version" {
+  type        = string
+  default     = "1.17"
+  description = "Desired Kubernetes master version. If you do not specify a value, the latest available version is used"
+}
+
+variable "enabled_cluster_log_types" {
+  type        = list(string)
+  default =  ["audit"]
+  description = "A list of the desired control plane logging to enable. For more information, see https://docs.aws.amazon.com/en_us/eks/latest/userguide/control-plane-logs.html. Possible values [`api`, `audit`, `authenticator`, `controllerManager`, `scheduler`]"
+}
+
+variable "cluster_log_retention_period" {
+  type        = number
+  default     = 7
+  description = "Number of days to retain cluster logs. Requires `enabled_cluster_log_types` to be set. See https://docs.aws.amazon.com/en_us/eks/latest/userguide/control-plane-logs.html."
+}
+
+variable "map_additional_aws_accounts" {
+  description = "Additional AWS account numbers to add to `config-map-aws-auth` ConfigMap"
+  type        = list(string)
+  default     = []
+}
+
+variable "map_additional_iam_roles" {
+  description = "Additional IAM roles to add to `config-map-aws-auth` ConfigMap"
+
+  type = list(object({
+    rolearn  = string
+    username = string
+    groups   = list(string)
+  }))
+
+  default = []
+}
+
+variable "map_additional_iam_users" {
+  description = "Additional IAM users to add to `config-map-aws-auth` ConfigMap"
+
+  type = list(object({
+    userarn  = string
+    username = string
+    groups   = list(string)
+  }))
+
+  default = []
+}
+
+variable "oidc_provider_enabled" {
+  type        = bool
+  default     = true
+  description = "Create an IAM OIDC identity provider for the cluster, then you can create IAM roles to associate with a service account in the cluster, instead of using `kiam` or `kube2iam`. For more information, see https://docs.aws.amazon.com/eks/latest/userguide/enable-iam-roles-for-service-accounts.html"
+}
+
+variable "local_exec_interpreter" {
+  type        = list(string)
+  default     = ["/bin/sh", "-c"]
+  description = "shell to use for local_exec"
+}
+
+####################################################################
+# EKS 
+####################################################################
+
+variable "kubernetes_labels" {
+  type        = map(string)
+  description = "Key-value mapping of Kubernetes labels. Only labels that are applied with the EKS API are managed by this argument. Other Kubernetes labels applied to the EKS Node Group will not be managed"
+  default     = {}
+}
+
+variable "cluster_encryption_config_enabled" {
+  type        = bool
+  default     = true
+  description = "Set to `true` to enable Cluster Encryption Configuration"
+}
+
+variable "cluster_encryption_config_kms_key_id" {
+  type        = string
+  default     = ""
+  description = "KMS Key ID to use for cluster encryption config"
+}
+
+variable "cluster_encryption_config_kms_key_enable_key_rotation" {
+  type        = bool
+  default     = true
+  description = "Cluster Encryption Config KMS Key Resource argument - enable kms key rotation"
+}
+
+variable "cluster_encryption_config_kms_key_deletion_window_in_days" {
+  type        = number
+  default     = 10
+  description = "Cluster Encryption Config KMS Key Resource argument - key deletion windows in days post destruction"
+}
+
+variable "cluster_encryption_config_kms_key_policy" {
+  type        = string
+  default     = null
+  description = "Cluster Encryption Config KMS Key Resource argument - key policy"
+}
+
+variable "cluster_encryption_config_resources" {
+  type        = list(any)
+  default     = ["secrets"]
+  description = "Cluster Encryption Config Resources to encrypt, e.g. ['secrets']"
+}
+
+####################################################################
+# EKS Worker Groups 
+####################################################################
+
+variable "autoscaling_policies_enabled" {
+  type        = bool
+  default = true
+  description = "Whether to create `aws_autoscaling_policy` and `aws_cloudwatch_metric_alarm` resources to control Auto Scaling"
+}
+
+variable "eks_worker_groups" {
+  description = "EKS Worker Groups"
+  type = list(object({
+    name = string
+    instance_type  = string
+    desired_size  = number
+    min_size  = number
+    max_size  = number
+  }))
+  default = [
+    {
+      name = "t3a_medium"
+      instance_type = "t3a.medium"
+      desired_size = 1
+      min_size = 1
+      max_size = 2
+    }
+  ]
+}
+
+####################################################################
+# EKS Node Groups 
+####################################################################
+
+variable "eks_node_groups" {
+  type = list(object({
+    instance_types   = list(string)
+    desired_size  = number
+    min_size  = number
+    max_size  = number
+    disk_size  = number
+  }))
+  description = "EKS Worker Groups"
+  default = [
+    {
+      instance_types = ["t3a.medium"]
+      desired_size = 1
+      min_size = 1
+      max_size = 2
+      disk_size = 20
+    }
+  ]
 }
